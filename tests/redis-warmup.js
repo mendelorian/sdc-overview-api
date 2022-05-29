5b65b3081dc6128adb0393663ffe27c3c9579ea8
mendelorian <ipanda303@gmail.com>
2022-05-29 14:54:10 -0700

    Adjusted test duration

--- tests/redis-warmup.js
+++ tests/redis-warmup.js
@@ -2,7 +2,7 @@ const http = require('k6/http');
 const { sleep } = require('k6');
 export const options = {
   vus: 1000,
-  duration: '2m'
+  duration: '3m'
 };
 
 
@@ -11,6 +11,8 @@ export default function () {
   var productId = Math.floor(Math.random() * (1000011 - lowerVal) + lowerVal);
   // var productId = Math.floor(Math.random() * 1000011);
 
-  http.get(`http://localhost:8080/products/${productId}/styles`);
+  http.get(`http://54.67.77.142/products/${productId}/styles`);
+  // http.get(`http://13.57.29.232:8080/products/${productId}/styles`);
+
   sleep(1);
 }
